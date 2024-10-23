9581afde74ee784e2ace989ec72730b3c144c15d
ThomasKaar <31984130+ThomasKaar@users.noreply.github.com>
2024-10-23 23:33:46 +0200

    Add Development Tooling (#8)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,10 +5,28 @@
 //
 // SPDX-License-Identifier: MIT
 //
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite({
+      routeTreeFileHeader: [
+        `//
+// This source file is part of the Stanford Biodesign Digital Health RadGPT open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//`,
+        '/* prettier-ignore-start */',
+        '/* eslint-disable */',
+        '// @ts-nocheck',
+        '// noinspection JSUnusedGlobalSymbols',
+      ],
+    }),
+    react(),
+  ],
+});
