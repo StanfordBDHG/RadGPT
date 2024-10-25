85a79c6a7d72de3cbeed46fac8e250fb67f9ed8d
Thomas Kaar <thomas.kaar@gmail.com>
2024-10-25 14:34:20 +0200

    WIP add storage
    stack-info: PR: https://github.com/StanfordBDHG/RadGPT/pull/16, branch: ThomasKaar/stack/16

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -6,8 +6,11 @@
 # SPDX-License-Identifier: MIT
 #
 
+import pathlib
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from firebase_functions import storage_fn
+
 
 initialize_app()
 
@@ -15,3 +18,11 @@ initialize_app()
 @https_fn.on_request()
 def on_request_example(_req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world!")
+
+
+@storage_fn.on_object_finalized()
+def on_object_finalization(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
+    bucket_name = event.data.bucket
+    file_path = pathlib.PurePath(event.data.name)
+    content_type = event.data.content_type
+    print(bucket_name, file_path, content_type)
--- web/src/routes/protected_page.tsx
+++ web/src/routes/protected_page.tsx
@@ -5,18 +5,57 @@
 //
 // SPDX-License-Identifier: MIT
 //
+
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { auth } from "@utils/firebase.ts";
+import { auth, storage } from "@utils/firebase.ts";
 import useAuthenticatedUser from "../hooks/useAuthenticatedUser";
+import {
+  Button,
+  Field,
+  Input,
+  useForm,
+} from "@stanfordbdhg/spezi-web-design-system";
+import { ref, uploadBytes } from "firebase/storage";
+import { z } from "zod";
+
+const formSchema = z.object({
+  filePath: z.string().min(1, "Medical report is required"),
+});
 
 const ProtectedComponent = () => {
   const currentUser = useAuthenticatedUser();
+  const form = useForm({
+    formSchema,
+  });
+
+  const handleSubmit = form.handleSubmit(async (_, e) => {
+    const file = e?.target?.filePath?.files?.[0];
+    const storageReference = ref(
+      storage,
+      `reports/${auth.currentUser?.uid}/${file.name}`,
+    );
+    await uploadBytes(storageReference, file);
+  });
+
   return (
     <>
       <h1 className="text-3xl">Protected Screen</h1>
       <p className="text-s py-2">
         {currentUser?.email ?? "no user authenticated"}
       </p>
+      <form action="submit" onSubmit={handleSubmit}>
+        <Field
+          control={form.control}
+          name="filePath"
+          label={"Medical Report"}
+          render={({ field }) => {
+            return <Input type="file" accept=".txt,.pdf" {...field} />;
+          }}
+        />
+        <Button type="submit" isPending={form.formState.isSubmitting}>
+          Submit
+        </Button>
+      </form>
     </>
   );
 };
