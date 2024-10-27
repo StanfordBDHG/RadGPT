230514c839193a7ec9a73f76910f5c1d3ca0a75c
ThomasKaar <31984130+ThomasKaar@users.noreply.github.com>
2024-10-27 15:02:41 +0100

    Initilalize Firebase Emulator Ecosystem with Basic Firebase Function (#10)

---
+++ firebase/functions/main.py
@@ -0,0 +1,16 @@
+#
+# This source file is part of the Stanford Biodesign Digital Health RadGPT open-source project
+#
+# SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+#
+# SPDX-License-Identifier: MIT
+#
+from firebase_functions import https_fn
+from firebase_admin import initialize_app
+
+initialize_app()
+
+
+@https_fn.on_request()
+def on_request_example(_req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
