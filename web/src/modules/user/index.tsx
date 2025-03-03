1b590a526d19177081809b6c23d09b09744b38c6
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-03-03 19:40:08 +0100

    Fix compliance

--- web/src/modules/user/index.tsx
+++ web/src/modules/user/index.tsx
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health RadGPT open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { useNavigate } from "@tanstack/react-router";
 import { type User } from "firebase/auth";
 import { createContext, useContext, useState } from "react";
