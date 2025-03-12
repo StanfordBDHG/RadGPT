b543ced721a3c3defb3a0fc87e8db8635a15f10b
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-03-12 23:37:03 +0100

    Fix compliance

--- web/src/modules/firebase/getCallables.ts
+++ web/src/modules/firebase/getCallables.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health RadGPT open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { type Functions, httpsCallable } from "@firebase/functions";
 
 export interface OnDetailedExplanationRequestInput {
